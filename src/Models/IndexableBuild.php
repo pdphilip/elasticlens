0cf84065c76e1612c0f6e19236eb3a575ea73f8d
David Philip <pd.philip@gmail.com>
2025-09-04 01:31:40 +0200

    @phpstan-ignore

--- src/Models/IndexableBuild.php
+++ src/Models/IndexableBuild.php
@@ -104,6 +104,7 @@ class IndexableBuild extends Model
             $stateModel->index_model = $indexModel;
         }
         $stateModel->state = $state;
+        // @phpstan-ignore-next-line
         $stateModel->state_data = is_array($stateData) ? $stateData : [];
         $stateModel->last_source = $source;
         $logs = $stateModel->_prepLogs($stateData, $source);
